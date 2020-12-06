862c7dd772590f7aeb60f85d2cfb748ad6cf2fb9
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2020-12-06 23:38:04 +0100

    demoData added

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import demoData from './inst.json';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    recentFeeds: demoData.graphql.user.edge_owner_to_timeline_media.edges,
   },
   mutations: {
   },
